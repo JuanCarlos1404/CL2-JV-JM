d5a6dcde884e66d44e01766c9bd56f673d1f5db1
Juan Carlos <144704177+JuanCarlos1404@users.noreply.github.com>
2024-10-07 20:55:12 -0500

    Add files via upload

---
+++ src/main/java/cibertec/edu/pe/T2_JV_JM/T2JvJmApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.T2_JV_JM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2JvJmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2JvJmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/T2_JV_JM/T2JvJmApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.T2_JV_JM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2JvJmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
